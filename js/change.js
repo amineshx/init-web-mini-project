fd8d775c76d163a75b5d32ed37ae651b0f56f77a
Amine shx <98875453+amineshx@users.noreply.github.com>
2023-05-27 20:01:11 +0100

    Add files via upload

---
+++ js/change.js
@@ -0,0 +1,30 @@
+function change() {
+    function getCookie(name) {
+        const cDecoded = decodeURIComponent(document.cookie);
+        const cArray = cDecoded.split("; ");
+        console.log(cArray);
+        let Username = cArray[0].split("=");
+        Username = Username[1];
+        return Username
+    }
+    let usernameToFind = getCookie("loggedInUser");
+
+    var users = JSON.parse(localStorage.getItem('users'));
+    var foundUser = null;
+    var j = -1; // userindex in the users array objects
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].username === usernameToFind) {
+            foundUser = users[i];
+            j = i;
+            break;
+        }
+    }
+    let newusername = document.getElementById("newname").value;
+    let newemail = document.getElementById("newemail").value;
+    foundUser.username = newusername;
+    foundUser.email = newemail;
+    users[j] = foundUser;
+    localStorage.setItem('users', JSON.stringify(users));
+    alert("your data has changed ");
+    window.location.href= "profile.html";
+}
